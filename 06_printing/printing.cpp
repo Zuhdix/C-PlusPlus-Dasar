1dbcf3c5c7977a6763e5c94b33fd8e1e50e0cf83
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-01-18 21:35:22 +0700

    deskripsi baris

--- 06_printing/printing.cpp
+++ 06_printing/printing.cpp
@@ -2,6 +2,9 @@
 
 int main(){
 	std::cout << "halo sodara-sodara sekalian\n";
+	/* akhir baris bisa menggunakan \n
+ 	dan endl, sama dan tidak pengaruh
+	*/ 
 	std::cout << "ini adalah baris baru.";
 	std::cout << "ini bukan baris baru.";
 	std::cout << " ini akhir kalimat." << std::endl;
